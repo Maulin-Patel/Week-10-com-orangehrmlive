096b49362abd711d9d614c3f16a326a14ce628aa
Maulin-Patel <sareejunction@gmail.com>
2023-04-10 13:36:48 +0100

    Homework Week 10 com-orangehrmlive

---
+++ src/MultiBrowserTest.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class MultiBrowserTest {
+
+    static String browser = "Chrome"; // Static variable
+    static WebDriver driver; // Storing WebDriver object to variable
+
+    public static void main(String[] args) {
+        // Nested if else opening correct browser dependent on user choice
+        if (browser.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        } else {
+            System.out.println("Incorrect Browser Name");
+        }
+        driver.get("https://opensource-demo.orangehrmlive.com/");// Opening provided url in Chrome Browser
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));// Telling browser to wait implicitly
+        System.out.println(driver.getTitle());// Get title of the web page
+        System.out.println(driver.getCurrentUrl());// Getting and printing current url of the page
+        System.out.println(driver.getPageSource());// Getting and printing current page source
+        driver.findElement(By.name("username")).sendKeys("maulin1@gmail.com");// Finding email field with name Locator and passing value to put into email field
+        driver.findElement(By.name("password")).sendKeys("12345");// Finding password field with name Locator and passing value to put into password field
+        driver.close(); // Close the browser
+    }
+}
